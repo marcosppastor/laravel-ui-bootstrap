b6a29bcb76e61cfe905efee6f94b88b9e48cf980
Virag Shah <shahvirag07@gmail.com>
2020-09-23 23:43:29 +0530

    fix resend email verification notification

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
                 <div class="card-body">
-                    @if (session('resent'))
+                    @if (session('status') == 'verification-link-sent')
                         <div class="alert alert-success" role="alert">
                             {{ __('A fresh verification link has been sent to your email address.') }}
                         </div>
